0bedb441e8fa64df5085f6d101a7134eb7c63f70
Sergey Minakov <naithar@icloud.com>
2020-08-14 22:39:30 +0300

    Initial Commit

---
+++ godot_plugin/godot_plugin.h
@@ -0,0 +1,13 @@
+//
+//  godot_plugin.h
+//  godot_plugin
+//
+//  Created by Sergey Minakov on 14.08.2020.
+//  Copyright © 2020 Godot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface godot_plugin : NSObject
+
+@end
